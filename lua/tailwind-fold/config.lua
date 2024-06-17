66709bd8e4ed33829933e1e3d93b276a3251498d
Razak Mo <moabdulrazak17@gmail.com>
2024-06-17 14:23:56 +0000

    chore: cleanup

--- lua/tailwind-fold/config.lua
+++ lua/tailwind-fold/config.lua
@@ -5,7 +5,7 @@ M.filetype_to_extension = {
 	svelte = "svelte",
 	astro = "astro",
 	vue = "vue",
-	tsx = "tsx",
+	typescriptreact = "tsx",
 	php = "php",
 	blade = "blade",
 	eruby = "erb",
@@ -28,14 +28,14 @@ M.classname_filetypes = {
 
 M.options = {
 	enabled = true,
-	-- Only fold when class string char count is more than 30. Folds everything by default.
+	-- Only fold when class string char count is more than min_chars. Folds everything by default.
 	min_chars = 0,
 	ft = {
 		"html",
 		"svelte",
 		"astro",
 		"vue",
-		"tsx",
+		"typescriptreact",
 		"php",
 		"blade",
 		"eruby",
