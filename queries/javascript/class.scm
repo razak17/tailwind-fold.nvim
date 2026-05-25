88beed022e0b3b597e58dcf207ec47023df01cb2
Abdul Razak <moabdulrazak17@gmail.com>
2026-05-25 12:04:06 +0000

    refactor: inherit duplicate tsx queries

--- queries/javascript/class.scm
+++ queries/javascript/class.scm
@@ -1,16 +1,1 @@
 ; inherits: tsx
-
-(call_expression
-  function: [
-    (identifier) @_ident
-    (member_expression
-      object: (identifier) @_object.ident)
-  ]
-  (#match? @_ident "(clsx|classnames|t[vw]|css|cva)")
-  (#eq? @_object.ident "tw")
-  arguments: [
-    ((arguments
-     (_)+) @tailwind.inner._args
-     (#set! @tailwind.inner._args "sort" "skip"))
-    (template_string) @tailwind.inner._str
-  ])
--- queries/tsx/class.scm
+++ queries/tsx/class.scm
@@ -7,3 +7,18 @@
     (jsx_expression
       (template_string) @tailwind.inner)
   ])
+
+(call_expression
+  function: [
+    (identifier) @_ident
+    (member_expression
+      object: (identifier) @_object.ident)
+  ]
+  (#match? @_ident "(clsx|classnames|cn|t[vw]|css|cva)")
+  (#eq? @_object.ident "tw")
+  arguments: [
+    ((arguments
+     (_)+) @tailwind.inner._args
+     (#set! @tailwind.inner._args "sort" "skip"))
+    (template_string) @tailwind.inner._str
+  ])
