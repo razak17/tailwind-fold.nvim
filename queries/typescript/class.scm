24a7770be4676e633ad679d1f859db487c5baf13
Abdul Razak <moabdulrazak17@gmail.com>
2026-05-25 12:04:37 +0000

    feat: conceal cn classnames ▣  Build · GPT-5.4 · 13.7s

--- queries/typescript/class.scm
+++ queries/typescript/class.scm
@@ -4,7 +4,7 @@
     (member_expression
       object: (identifier) @_object.ident)
   ]
-  (#match? @_ident "(clsx|classnames|t[vw]|css|cva)")
+  (#match? @_ident "(clsx|classnames|cn|t[vw]|css|cva)")
   (#eq? @_object.ident "tw")
   arguments: [
     ((arguments
